08a5a375bd6d28cdeae72c7d47382cc74230a802
Cesahz <espinolac999@gmail.com>
2026-03-07 20:26:54 -0300

    reemplazar DFS por generador probabilistico clasico y despeje de spawns

--- algoritmos.py
+++ algoritmos.py
@@ -1,41 +1,25 @@
 import random
 from config import *
 
-def generar_laberinto_dfs(matriz,ancho,alto):
-    # rellenar todo el vacio con ladrillos
+def generar_mapa_bomberman(matriz, ancho, alto):
+    # rellenar con probabilidad
     for y in range(alto):
         for x in range(ancho):
             if matriz[y][x] == VACIO:
-                matriz[y][x] = MURO_LADRILLO
-    # DFS para tallar ruta principal
-    visitado = set()
+                if random.random() < 0.75:
+                    matriz[y][x] = MURO_LADRILLO
+
+    # despejar zonas de inicio para los jugadores
+    zonas_seguras = [
+        #esquina superior izquierda
+        (1, 1), (2, 1), (1, 2),
+        #esquina inferior derecha
+        (ancho-2, alto-2), (ancho-3, alto-2), (ancho-2, alto-3),
+        #esquina superior derecha
+        (ancho-2, 1), (ancho-3, 1), (ancho-2, 2),
+        #esquina inferior izquierda
+        (1, alto-2), (2, alto-2), (1, alto-3)
+    ]
     
-    def dfs(x,y):
-        visitado.add((x,y))
-        matriz[y][x] = VACIO 
-        
-        #direciones, saltos de a 2
-        direcciones = [(0, -2), (0, 2), (-2, 0), (2, 0)]
-        random.shuffle(direcciones) # aleatorizar para variedad de mapas
-        
-        for dx, dy in direcciones:
-            nx = x + dx
-            ny = y + dy
-            # validar limites
-            if 0 < nx < ancho-1 and 0 < ny < alto-1 and (nx, ny) not in visitado:
-                # tallar el muro intermedio
-                matriz[y + dy//2][x + dx//2] = VACIO
-                dfs(nx, ny)
-    #iniciar dfs desde player 1
-    dfs(1,1)
-    
-    #dinamitar muros extra
-    for y in range(1, alto-1):
-        for x in range(1, ancho-1):
-            if matriz[y][x] == MURO_LADRILLO and random.random() < 0.4:
-                matriz[y][x] = VACIO
-    
-    #despejar zonas de spawn
-    zonas_seguras = [(1,1), (1,2), (2,1), (ancho-2, alto-2), (ancho-3, alto-2), (ancho-2, alto-3)]
     for zx, zy in zonas_seguras:
         matriz[zy][zx] = VACIO
