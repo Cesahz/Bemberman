dadf92823a873aaf869911746db891df359c8232
Cesahz <espinolac999@gmail.com>
2026-03-07 20:37:55 -0300

    implementar radar predictivo de zonas de peligro

--- algoritmos.py
+++ algoritmos.py
@@ -23,3 +23,27 @@ def generar_mapa_bomberman(matriz, ancho, alto):
     
     for zx, zy in zonas_seguras:
         matriz[zy][zx] = VACIO
+
+def obtener_mapa_peligro(tablero, bombas_activas):
+    peligro = set()
+    for bomba in bombas_activas:
+        peligro.add((bomba.x, bomba.y)) # el centro de la bomba es peligroso
+        # 4 direcciones
+        direcciones = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        for dx, dy in direcciones:
+            for paso in range(1, bomba.rango + 1):
+                nx = bomba.x + (dx * paso)
+                ny = bomba.y + (dy * paso)
+                
+                #si sale del mapa se corta 
+                if nx < 0 or nx >= tablero.ancho or ny < 0 or ny >= tablero.alto:
+                    break
+                
+                celda = tablero.matriz[ny][nx]
+                if celda == MURO_ACERO:
+                    break
+                peligro.add((nx, ny)) #zona de muerte
+                
+                if celda == MURO_LADRILLO:
+                    break # absorbe la explosion jeje
+    return peligro
